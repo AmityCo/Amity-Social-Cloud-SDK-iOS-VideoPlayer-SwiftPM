42070aee8b2d53c915afbd25b367cd7e4903306d
Zryte <>
2023-03-30 16:30:37 +0700

    update Package.swift for v6.5.0

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "AmityVideoPlayer",
+    products: [
+        .library(
+            name: "AmityVideoPlayer",
+            targets: ["AmityVideoPlayerKit", "MobileVLCKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "AmityVideoPlayerKit",
+            url: "https://sdk.amity.co/sdk-release/ios-frameworks/6.5.0/AmityVideoPlayerKit.xcframework.zip",
+            checksum: "4e5eb335e2bb2d60ac954424cc25876fe4e0399215094d6aa31e9579a4b10108"
+        ),
+        .binaryTarget(
+            name: "MobileVLCKit",
+            url: "https://sdk.amity.co/sdk-release/ios-frameworks/5.1.0/MobileVLCKit.xcframework.zip",
+            checksum: "64e78ecdf0657246ac047b995d86a890a1c78852be968f5d80de2b28c90dc1a9"
+        ),
+    ]
+)
